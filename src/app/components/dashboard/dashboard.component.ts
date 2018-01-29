aaecd734e010a0e896928a41fc353dedc92ece3f
RemiFrontiere <remi.frontiere@gmail.com>
2018-01-29 14:54:00 +0100

    UK lang trad

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -18,7 +18,6 @@ export class DashboardComponent implements OnInit{
 
   public selectedProject : Project;
   public selectedChip = "Tout";
-  public chip1 = 'Tout';
   public chip2 = 'Web';
   public chip3 = 'Mobile';
 
@@ -33,16 +32,26 @@ export class DashboardComponent implements OnInit{
   }
 
   public getJson(){
-    this.http.get('../../../assets/JSON/projects.json').subscribe(data => {
-      for (let i of data['projects']) {
-        this.myProjects.push(new Project(i["id"], i["name"], i["language"], i["topimg"], i["texts"]));
-      }
-      this.selectedProject = this.myProjects[0];
-    });
     this.http.get('../../../assets/JSON/competences.json').subscribe(data => {
       for (let i of data['competences']) {
         this.myCompetences.push(new Competence(i["id"], i["type"], i["icon"], i["lvl"]));
       }
     });
+    if(this.selectedLang == 'fr'){
+      this.http.get('../../../assets/JSON/projects.json').subscribe(data => {
+        for (let i of data['projects']) {
+          this.myProjects.push(new Project(i["id"], i["name"], i["language"], i["topimg"], i["texts"]));
+        }
+        this.selectedProject = this.myProjects[0];
+      });
+    }
+    else if(this.selectedLang == 'uk'){
+      this.http.get('../../../assets/JSON/projectsUK.json').subscribe(data => {
+        for (let i of data['projects']) {
+          this.myProjects.push(new Project(i["id"], i["name"], i["language"], i["topimg"], i["texts"]));
+        }
+        this.selectedProject = this.myProjects[0];
+      });
+    }
   }
 }
